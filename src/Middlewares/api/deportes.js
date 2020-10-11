8ee2e609a3749b5fb93ccd460b818dcbe411ebe2
Daniel Villalobos <dandiivc@gmail.com>
2020-10-11 20:47:08 -0300

    Conexion BBDB, sincronizar base de datos, modelo deporte, api deportes V2.

--- src/Middlewares/api/deportes.js
+++
@@ -1,5 +0,0 @@
-const { check, validationResult } = require('express-validator');
-
-exports.deporteValidator = async (req) => {
-    [check('nombre_deporte', 'El nombre del deporte es un campo obligatorio')]
-}
