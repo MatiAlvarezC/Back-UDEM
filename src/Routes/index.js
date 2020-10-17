e8fb5d5879c792503a46bfef82793427b14be728
Marco Gutierrez <64593778+MGutierrezT@users.noreply.github.com>
2020-10-16 21:52:17 -0300

    +Fix de declaración de rutas

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -2,12 +2,14 @@ const Status_R = require('./Status_Routes');
 const User_R = require('./User_Routes')
 const Player_R = require('./Player_Routes')
 const Campus_R = require('./Campus_Routes')
+const Program_R = require('./Program_Routes')
 
 require("../models/Relations/Relations")
 
 module.exports = (app) => {
-    app.use('/',Status_R);
     app.use('/user', User_R);
     app.use('/player',Player_R)
     app.use('/campus',Campus_R)
+    app.use('/program',Program_R)
+    app.use('/status',Status_R)
 }
