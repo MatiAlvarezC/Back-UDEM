ad8a7262cec692ec2a8f9f5f27b5ae133a4abfde
MGutierrez <m.gutierrez.tolorza@gmail.com>
2020-11-16 15:35:04 -0300

    +Test

--- src/Controllers/Test_Controller.js
+++ src/Controllers/Test_Controller.js
@@ -31,7 +31,7 @@ const sendEmail = async (req, res) => {
 }
 
 const test = async (req,res) => {
-    return res.send("test webhook 3")
+    return res.send("test webhook 4")
 }
 
 module.exports = {
